cdc38ad2206550ca2f5c649b1c67b6ab8bd8c8c1
mpachkov <pachkov@gmail.com>
2021-05-27 17:22:25 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# crema_uploader
+# Upload client for the CREMA web-service (https://crema.unibas.ch)
+
+This is a python script which uploads your files to the CREMA
+web-server and starts the CREMA analysis.
+
+All you need to do is to download **crema_uploader.py** script and start using it.
+
+**Important!** This script requires Python3 as well as the *requests* library.
+
+## Purpose
+
+This script is aimed to users who has their data files stored on remote machines (usually linux) which can use this script to upload their data without use of an internet browser. So you do not need to copy files to your local machine for uploading to the ISMARA server.
+
+This is beta version to see if there are people interested in such application, We hope to get user feedback to properly shape the further development.
+
+**All feedback is welcome!**
+
+## Usage
+
+```shell
+python crema_uploader.py [-h] [-e EMAIL] [-p PROJECT]
+                         [-t {microarray,rnaseq,chipseq,cage}]
+                         [-o {hg18,mm9,hg19,mm10}]
+                         --file-list TSV_FILE
+```
+
+### Optional arguments:
+
+* -h, --help :  show this help message and exit
+* -e EMAIL |: email address
+* -p PROJECT : project name
+* -t : data type {chip-seq, atac-seq}, default: chip-seq
+* --file-list : TSV file list of files, ascii text, one line per file path
+
+#### File format support
+The following file formats are supported:
+* **rnaseq/chipseq** : .fastq[.gz] 
+
+#### TSV file format
+The .tsv file contains paths to fastq files for uploading and annotation of these files i.e. to chich condition and which type each file belongs. All values in the file are separated with tabs. Header line is required.
+Here is an example:
+```
+sample	type	fq1	fq2
+condition1	fg	/data/file1.fastq.gz
+condition1	bg	/data/file2.fastq.gz
+condition2	fg      /data/file3_R1.fastq.gz	/data/file3_R2.fastq.gz
+condition2      bg	/data/file4_R1.fastq.gz	/data/file4_R2.fastq.gz
+condition3	fg	/data/file5.fastq.gz
+condition3	fg	/data/file6.fastq.gz
+condition3	fg	/data/file7.fastq.gz
+condition3	bg	/data/file8.fastq.gz
+condition3	bg	/data/file9.fastq.gz
+```
+
+First column contains condition name for every fastq file (fastq pair). Second column contains type of a file either **fg** for foreground samples or **bg** for background samples. Third column contains path to fastq file or path fastq file for firs end reads in a pair. Fourth column contains path a second end read file iof data is paired end.
+If you have data with multiple replicates per condition you can specify multiple fastq files percondition like in the example ebove for *condition3*. Your dataset could be a mixture of single-end and paired end fastq files. 
+
+### Example
+
+2. We run the script in background:
+```shell
+python crema_uploader.py -e user@example.com -p "my cool project" -data-type chip-seq -o hg19 \
+    --file-list file_list.txt 1>crema_uploader.out 2>crema_uploader.err &
+```
+
+3. In the file crema_uploader.out the last lines contain a link to status page of your submission. If you submitted your email address then you will get a notification once your job is finished.
---
+++ crema_uploader.py
@@ -0,0 +1,160 @@
+""" Uploads files to CREMA webserver """
+
+import argparse
+import json
+import logging
+import os
+import requests
+import time
+import shutil
+import sys
+import tempfile
+
+UPLOAD_URL = "https://crema.unibas.ch/crumara/upload"
+RUN_URL = "https://crema.unibas.ch/crumara/run"
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Uploads files to CREMA webserver.')
+    parser.add_argument('-e',
+                        help='email address',
+                        dest='email',
+                        default='')
+    parser.add_argument('-p',
+                        help='project name',
+                        dest='project',
+                        default='')
+    parser.add_argument('--data-type',
+                        help='data type',
+                        dest='data_type',
+                        choices=["chip-seq", "atac-seq"],
+                        default='chip-seq')
+    parser.add_argument('-o',
+                        help='Organism id: hg19 for human, mm10 for mouse, rb6 for rat',
+                        dest='organism',
+                        choices=['hg19', 'mm10', 'rn6'],
+                        default='hg19')
+
+    parser.add_argument('--file-list',
+                        help="TSV_FILE containing information on sample, sample type and file path",
+                        dest="file_list",
+                        required=True)
+    
+    args = parser.parse_args()
+
+    upload_session = requests.Session()
+    upload_session.verify = True
+    upload_session.timeout = (10, 600)
+    save_dir = get_sd(upload_session)
+
+    # save user parameters
+    job_data = {"email": args.email,
+                "project": args.project,
+                "organism": args.organism,
+                "datatype": args.data_type,
+                "submission": "uploader"}
+
+    # save job parameters in advance
+    try:
+        r =  upload_session.post("https://crema.scicore.unibas.ch/crumara/save_json",
+                                 data={"sd": save_dir,
+                                       "data":json.dumps(job_data)})
+        logging.warning("Job information is sent to the server.")
+    except:
+        logging.warning("Error: Could not save user parameters!\n%s!", str(sys.exc_info()))
+
+    files = set()
+    new_content = []
+    with open(args.file_list) as fin:
+        # read header
+        content = [line.strip().split("\t") for line in fin]
+        f1_idx = content[0].index("fq1")
+        if "fq2" in content[0]:
+            f2_idx = content[0].index("fq2")
+        else:
+            f2_idx = None
+        new_content.append("\t".join(content[0]))
+        for data in content[1:]:
+            filepath = data[f1_idx].strip()
+            files.add(filepath)
+            filename = os.path.split(filepath)[1]
+            data[f1_idx] = filename
+            if f2_idx is not None and len(data) > f2_idx:
+                filepath = data[f2_idx].strip()
+                if filepath != "":
+                    files.add(filepath)
+                    filename = os.path.split(filepath)[1]
+                    data[f2_idx] = filename
+            new_content.append("\t".join(data))
+    
+    #  make temp dir to keep modified samples.tsv for sending to server
+    temp_dir = tempfile.mkdtemp(prefix=".crema_", dir="./")
+    with open("%s/samples.tsv" % temp_dir, "wt") as fout:
+        fout.write("\n".join(new_content))
+    files.add("%s/samples.tsv" % temp_dir)
+
+    for f in files:
+        logging.warning("uploading file %s", f)
+        with open(f, 'rb') as fin:
+            index=0
+            headers={}
+            content_size = os.path.getsize(f)
+            file_name = os.path.basename(f)
+            for chunk in read_in_chunks(fin):
+                offset = index + len(chunk)
+                headers['Content-Type'] = 'application/octet-stream'
+                headers['Content-length'] = str(content_size)
+                headers['Content-Range'] = 'bytes %s-%s/%s' % (index, offset, content_size)
+                index = offset
+                error = ''
+                for i in range(10):
+                    try:
+                        r =  upload_session.post(UPLOAD_URL, files={"files[]":(file_name, chunk)},
+                                                 data={"sd": save_dir})
+                        error = ''
+                        break
+                    except:
+                        error = sys.exc_info()[0]
+                        logging.warning("Error: %s!!!\nRetrying to re-upload data. Retry %d", str(sys.exc_info()), i+1)
+                        time.sleep(60)
+                if error != '':
+                    logging.warning("Could not upload the data! Please report this id to CREMA administrators: %s!", save_dir)
+        logging.warning("Finished uploading file %s", f)
+
+    # remove temp dir after upload
+    if os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
+    
+    # init job run after upload
+    email = args.email
+    project = args.project
+    organism = args.organism
+    data_type = args.data_type
+    r = upload_session.post(RUN_URL, data={"sd": save_dir,
+                                           "email": email,
+                                           "project": project,
+                                           "method": "crema_uploader",
+                                           "datatype": data_type,
+                                           "organism": organism})
+    print("\n>>>>>>>>>>\nHere is link to your results:\n    %s" % (r.text.strip()))
+
+
+def get_sd(mysession):
+    """ get name of working directory for a project """
+    url = "https://crema.scicore.unibas.ch/crumara/get_sd"
+    res = mysession.get(url)
+    res = [x for x in res.iter_lines()][0].decode("utf-8")
+    return(res)
+
+
+def read_in_chunks(file_object, chunk_size=50000000):
+    """ Read file in chuncks """
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        yield data
+
+
+if __name__ == "__main__":
+    main()
